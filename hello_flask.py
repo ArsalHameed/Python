cfdd828e87abe3733213814bf595dbcdf5021c93
Arsal <40295496+ArsalHameed@users.noreply.github.com>
2025-08-25 23:25:52 +0500

    ch5 import module vfinalsearch

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+from vfinalsearch import search4letters
 app = Flask(__name__)
 
 @app.route("/")   # This maps the root URL (http://127.0.0.1:5001/) to the function
 def hello() -> str:
     return "hello world from Flask!"
+
+@app.route('/search4')
+def do_search()-> str:
+    return str(search4letters('life, the universe, and everything','eiru,!'))
 
 if __name__ == "__main__":
     app.run(port=5001)
