9537bb88caba81ce80499e4e06e00a564ae0cd20
Arsal <40295496+ArsalHameed@users.noreply.github.com>
2025-08-26 23:28:18 +0500

    Ch5 Project: letters in phrase

--- hello_flask.py
+++ hello_flask.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from vfinalsearch import search4letters
+
 app = Flask(__name__)
 
 @app.route("/")   # This maps the root URL (http://127.0.0.1:5001/) to the function
 def hello() -> str:
     return "hello world from Flask!"
 
-@app.route('/search4')
-def do_search()-> str:
-    return str(search4letters('life, the universe, and everything','eiru,!'))
+@app.route('/search4', methods=['POST'])
+def do_search() -> str:
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    
+    the_title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    
+    return render_template(
+        'results.html',
+        the_title=the_title,
+        the_results=results,
+        the_letters=letters,
+        the_phrase=phrase,
+    )
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template(
+        'entry.html',
+        the_title='Welcome to search4letters on the web!'
+    )
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(port=5001, debug=True)
