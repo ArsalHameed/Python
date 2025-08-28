ea583c90400fd028f62a1e14fb1e2b3aabb27cb8
Arsal <40295496+ArsalHameed@users.noreply.github.com>
2025-08-28 23:37:43 +0500

    Ch6 from markupsafe import escape

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request,escape
+from flask import Flask, render_template, request
+from markupsafe import escape
 from vfinalsearch import search4letters
 
 app = Flask(__name__)
